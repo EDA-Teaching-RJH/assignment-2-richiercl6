5cca9ac9ea26ba1a437d7cba5639913cc5421438
richiercl6 <rcl6@kent.ac.uk>
2024-10-28 10:05:42 +0000

    done

--- AllTogether.py
+++ AllTogether.py
@@ -1,1 +1,16 @@
 ### Part Four -- your code goes here. 
+import random
+list = [random.randint(1,100) for I in range(10)]
+print(list)
+
+index = 0 
+while index < len(list):
+    if list[index] % 2 == 0:
+        list.pop(index)
+    else:
+        index = index + 1
+
+print("odd numbers are",list)
+
+
+
